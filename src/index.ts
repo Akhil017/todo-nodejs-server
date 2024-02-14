3ba387ce03dac3aa252ea55844e6b2d6f314207f
Akhil017 <akhil.17.akhi@gmail.com>
2024-02-15 00:21:51 +0530

    basic folder structure and routes done

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import express, { Request, Response } from "express";
-const PORT = 7777;
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const PORT = process.env.PORT || 7777;
 const app = express();
+
+//routes
+app.use("/api/todo", require("./routes/todoRoutes"));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("hello there");
 });
 
-app.listen(PORT, () => `server running at ${PORT}`);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.listen(PORT, () => `server running on port ${PORT}`);
